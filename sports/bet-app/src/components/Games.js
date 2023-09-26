28dd3a339ee08770a196302f84e49aab22700629
dussedeleche <jasminewilliams92012@gmail.com>
2023-09-26 16:59:44 -0400

    wallet

--- sports/bet-app/src/components/Games.js
+++ sports/bet-app/src/components/Games.js
@@ -12,7 +12,7 @@ function Games() {
     async function fetchData() {
       try {
         if (!localStorage.getItem("matches")) {
-            const response = await axios.get("https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?apiKey=db674826a04c54b81c83dd4c0f1c48d1&regions=us&markets=h2h,spreads,totals&oddsFormat=american");
+            const response = await axios.get("https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?apiKey=f2fed7ad4248a33fd48f2a1a4e1ab672&regions=us&markets=h2h,spreads,totals&oddsFormat=american");
             localStorage.setItem("matches", JSON.stringify(response.data));
             setMatches(response.data);
             setLoading(false);
