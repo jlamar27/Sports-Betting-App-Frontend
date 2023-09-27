ae0e8789de9d69ae469162144debc8e38722c1f8
jmorgainz <joshua.morgan.elite@gmail.com>
2023-09-27 11:15:21 -0400

    added betValue to the bet slip

--- sports/bet-app/src/components/BetSlip.js
+++ sports/bet-app/src/components/BetSlip.js
@@ -22,20 +22,54 @@ function BetSlip({ betSlip, updateBetSlip }) {
   const handleBetValueChange = (index, value) => {
     setBetValues(prev => {
       const newBetValues = { ...prev };
-      newBetValues[index] = value;
+      newBetValues[index] = Number(value);
+      console.log(typeof(newBetValues[index]));
       return newBetValues;
     });
   };
 
+  const userId = localStorage.getItem('userId');
+
   const handlePlaceBet = async () => {
+    const token = localStorage.getItem('token')
     try {
-      // Replace with your API Endpoint and adjust the payload as needed.
-      const response = await axios.post('/api/placeBet', { bets: betSlip, betValues });
-      console.log(response.data);
-    } catch (error) {
-      console.error('Error placing bet', error);
+        const mergedBetSlip = betSlip.map((bet, index) => {
+          return {
+            ...bet,
+            betValue: betValues[index] || 0, // or use a default value other than 0 if needed
+          };
+        });
+    
+        console.log('mergedBetSlip', mergedBetSlip);
+
+        const response = await axios.post(
+          `http://localhost:3000/bet/${userId}`,
+          { bets: mergedBetSlip },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        
+        console.log(response.data);
+      } catch (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.error('Error response from server', error.response.data);
+        console.error('Error response status', error.response.status);
+        console.error('Error response headers', error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.error('No response received', error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.error('Error setting up the request', error.message);
+      }
     }
   };
+  
 
   const handleRemoveBet = (index) => {
     updateBetSlip(prev => prev.filter((_, i) => i !== index));
